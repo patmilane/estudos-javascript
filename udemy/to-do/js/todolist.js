5794db364263855706f152a6a701339ce79d21b7
scarleteyes1996 <patriciahmilane@hotmail.com>
2022-12-26 10:39:00 -0300

    projetinho com som

---
+++ udemy/to-do/js/todolist.js
@@ -0,0 +1,17 @@
+;(function(){
+
+    //armazenar dom em variaveis
+    const itemInput = document.getElementById("item-input")
+    const todoAddForm = document.getElementById("todo-add")
+    const ul = document.getElementById("todo-list")
+
+    todoAddForm.addEventListener("submit", function(e){
+        e.preventDefault()
+
+        ul.innerHTML = `<li class="todo-item">
+                            <p class="task-name">Example</p>
+                        </li>`
+
+    })
+
+})()
